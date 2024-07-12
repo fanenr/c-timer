c5609d03498f8bc0f4002bbeb27966d06b3b8b5b
Arthur63 <arthur63@163.com>
2024-07-13 02:04:13 +0800

    tweak timer_exec

--- timer.c
+++ timer.c
@@ -9,7 +9,10 @@
 #define PARENT(i) (((i) - 1) / 2)
 
 static long time_now (void);
+
 static void timer_pop (timer_mgr_t *mgr);
+#define timer_top(mgr) (mgr->size ? mgr->tasks : NULL)
+
 static timer_task_t *shift_up (timer_task_t *tasks, size_t i);
 static timer_task_t *shift_down (timer_task_t *tasks, size_t s);
 
@@ -23,9 +26,8 @@ timer_free (timer_mgr_t *mgr)
 void
 timer_exec (timer_mgr_t *mgr)
 {
-  for (; mgr->size;)
+  for (timer_task_t *top; (top = timer_top (mgr));)
     {
-      timer_task_t *top = mgr->tasks;
       if (top->expire > time_now ())
         break;
 
@@ -37,14 +39,16 @@ timer_exec (timer_mgr_t *mgr)
 int
 timer_recent (timer_mgr_t *mgr)
 {
-  if (!mgr->size)
-    return -1;
+  timer_task_t *top;
 
-  timer_task_t *top = mgr->tasks;
-  long expire = top->expire;
-  long now = time_now ();
+  if ((top = timer_top (mgr)))
+    {
+      long now = time_now ();
+      long expire = top->expire;
+      return expire > now ? expire - now : 0;
+    }
 
-  return expire > now ? expire - now : 0;
+  return -1;
 }
 
 timer_task_t *
@@ -77,12 +81,12 @@ timer_add (timer_mgr_t *mgr, int ms, timer_cb_t *cb, void *arg)
   return shift_up (tasks, mgr->size++);
 }
 
-#define swap(tasks, i, p)                                                     \
+#define swap(tasks, a, b)                                                     \
   do                                                                          \
     {                                                                         \
-      timer_task_t temp = tasks[i];                                           \
-      tasks[i] = tasks[p];                                                    \
-      tasks[p] = temp;                                                        \
+      timer_task_t temp = tasks[a];                                           \
+      tasks[a] = tasks[b];                                                    \
+      tasks[b] = temp;                                                        \
     }                                                                         \
   while (0)
 
@@ -101,23 +105,22 @@ timer_pop (timer_mgr_t *mgr)
   size_t size = mgr->size;
   timer_task_t *tasks = mgr->tasks;
 
-  if (!size)
-    return;
-
-  if (--size)
+  if (size && --size)
     {
       swap (tasks, 0, size);
       shift_down (tasks, size);
     }
 
-  mgr->size--;
+  --mgr->size;
 }
+
+#define less(tasks, a, b) (tasks[a].expire < tasks[b].expire)
+#define greater(tasks, a, b) (tasks[a].expire > tasks[b].expire)
 
 static inline timer_task_t *
 shift_up (timer_task_t *tasks, size_t i)
 {
-  for (size_t p; i && (p = PARENT (i), tasks[i].expire < tasks[p].expire);
-       i = p)
+  for (size_t p; i && (p = PARENT (i), less (tasks, i, p)); i = p)
     swap (tasks, i, p);
   return tasks + i;
 }
@@ -127,9 +130,9 @@ shift_down (timer_task_t *tasks, size_t s)
 {
   for (size_t i = 0, m, l, r;
        (m = i, l = LEFT (i), r = RIGHT (i),
-       m = l < s && tasks[m].expire > tasks[l].expire ? l : m,
-       m = r < s && tasks[m].expire > tasks[r].expire ? r : m, m != i);
+       m = l < s && greater (tasks, m, l) ? l : m,
+       m = r < s && greater (tasks, m, r) ? r : m, m != i);
        i = m)
-    swap (tasks, m, i);
+    swap (tasks, i, m);
   return tasks;
 }
