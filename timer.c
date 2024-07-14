4227ac8b365382b9a3be7619edd216348c889d54
Arthur63 <Arthur63@163.com>
2024-07-14 12:26:20 +0800

    update timer.c

--- timer.c
+++ timer.c
@@ -1,16 +1,16 @@
 #include "timer.h"
-#include <stdlib.h>
 
-#define TIMER_INIT_CAP 8
-#define TIMER_EXPAN_RATIO 2
+#include <stdbool.h>
+#include <stdlib.h>
 
 #define LEFT(i) ((i) * 2 + 1)
 #define RIGHT(i) ((i) * 2 + 2)
 #define PARENT(i) (((i) - 1) / 2)
 
-static long time_now (void);
+static size_t time_now (void);
 
 static void timer_pop (timer_mgr_t *mgr);
+static bool timer_expand (timer_mgr_t *mgr);
 #define timer_top(mgr) (mgr->size ? mgr->tasks : NULL)
 
 static timer_task_t *shift_up (timer_task_t *tasks, size_t i);
@@ -26,27 +26,19 @@ timer_free (timer_mgr_t *mgr)
 void
 timer_exec (timer_mgr_t *mgr)
 {
-  for (timer_task_t *top; (top = timer_top (mgr));)
-    {
-      if (top->expire > time_now ())
-        break;
-
-      top->cb (top->arg);
-      timer_pop (mgr);
-    }
+  for (timer_task_t *top;
+       (top = timer_top (mgr)) && top->expire <= time_now (); timer_pop (mgr))
+    top->cb (top->arg);
 }
 
 int
 timer_recent (timer_mgr_t *mgr)
 {
   timer_task_t *top;
+  size_t expire, now;
 
-  if ((top = timer_top (mgr)))
-    {
-      long now = time_now ();
-      long expire = top->expire;
-      return expire > now ? expire - now : 0;
-    }
+  if ((top = timer_top (mgr)) && (now = time_now ()))
+    return (expire = top->expire) > now ? expire - now : 0;
 
   return -1;
 }
@@ -54,31 +46,22 @@ timer_recent (timer_mgr_t *mgr)
 timer_task_t *
 timer_add (timer_mgr_t *mgr, int ms, timer_cb_t *cb, void *arg)
 {
-  long expire;
-  size_t cap = mgr->cap;
-  size_t size = mgr->size;
-  timer_task_t *tasks = mgr->tasks;
+  size_t expire;
 
-  if (size >= cap)
-    {
-      cap = cap ? cap * TIMER_EXPAN_RATIO : TIMER_INIT_CAP;
-      if (!(tasks = realloc (tasks, cap * sizeof (timer_task_t))))
-        return NULL;
-      mgr->tasks = tasks;
-      mgr->cap = cap;
-    }
+  if (!timer_expand (mgr))
+    return NULL;
 
-  if (ms <= 0 || (expire = time_now ()) == -1)
+  if (ms <= 0 || !(expire = time_now ()))
     return NULL;
   expire += ms;
 
-  tasks[size] = (timer_task_t){
+  mgr->tasks[mgr->size] = (timer_task_t){
     .expire = expire,
     .arg = arg,
     .cb = cb,
   };
 
-  return shift_up (tasks, mgr->size++);
+  return shift_up (mgr->tasks, mgr->size++);
 }
 
 #define swap(tasks, a, b)                                                     \
@@ -90,28 +73,48 @@ timer_add (timer_mgr_t *mgr, int ms, timer_cb_t *cb, void *arg)
     }                                                                         \
   while (0)
 
-static inline long
+static inline size_t
 time_now (void)
 {
   struct timespec ts;
+
   if (clock_gettime (CLOCK_MONOTONIC_COARSE, &ts) == 0)
-    return ts.tv_sec * 1000 + ts.tv_nsec / (1000 * 1000);
-  return -1;
+    return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+
+  return 0;
 }
 
 static inline void
 timer_pop (timer_mgr_t *mgr)
 {
-  size_t size = mgr->size;
+  if (mgr->size && --mgr->size)
+    {
+      swap (mgr->tasks, 0, mgr->size);
+      shift_down (mgr->tasks, mgr->size);
+    }
+}
+
+#define TIMER_INIT_CAP 8
+#define TIMER_EXPAN_RATIO 2
+
+static inline bool
+timer_expand (timer_mgr_t *mgr)
+{
+  size_t cap = mgr->cap;
   timer_task_t *tasks = mgr->tasks;
 
-  if (size && --size)
+  if (mgr->size < cap)
+    return true;
+
+  cap = cap ? cap * TIMER_EXPAN_RATIO : TIMER_INIT_CAP;
+  if ((tasks = realloc (tasks, cap * sizeof (timer_task_t))))
     {
-      swap (tasks, 0, size);
-      shift_down (tasks, size);
+      mgr->tasks = tasks;
+      mgr->cap = cap;
+      return true;
     }
 
-  --mgr->size;
+  return false;
 }
 
 #define less(tasks, a, b) (tasks[a].expire < tasks[b].expire)
@@ -120,7 +123,7 @@ timer_pop (timer_mgr_t *mgr)
 static inline timer_task_t *
 shift_up (timer_task_t *tasks, size_t i)
 {
-  for (size_t p; i && (p = PARENT (i), less (tasks, i, p)); i = p)
+  for (unsigned p; i && (p = PARENT (i), less (tasks, i, p)); i = p)
     swap (tasks, i, p);
   return tasks + i;
 }
@@ -128,7 +131,7 @@ shift_up (timer_task_t *tasks, size_t i)
 static inline timer_task_t *
 shift_down (timer_task_t *tasks, size_t s)
 {
-  for (size_t i = 0, m, l, r;
+  for (unsigned i = 0, m, l, r;
        (m = i, l = LEFT (i), r = RIGHT (i),
        m = l < s && greater (tasks, m, l) ? l : m,
        m = r < s && greater (tasks, m, r) ? r : m, m != i);
